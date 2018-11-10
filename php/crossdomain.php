8f95609b29577367b61a83c7d2c7b609ec7021dc
ChuanxiZhang <chuanxiz@usc.edu>
2018-11-10 01:48:09 -0800

    debug

--- php/crossdomain.php
+++ php/crossdomain.php
@@ -4,11 +4,7 @@
   $site = 'https://www.food2fork.com/api/';
 
   function url_strs(){
-<<<<<<< HEAD
-    $key = '87d8f0edf7265d772cdd0caefd565a3d';
-=======
     $key = '7634b54f5c093ea22c6edafe02f275bd';
->>>>>>> 382f892d5fc2fcd80bf0bf7b74c221d1152a627e
     if (isset($_GET['search'])) {
       $query = $_GET['search'];
       return "search?key=".$key."&q=".$query[0]."&page=".$query[1];
